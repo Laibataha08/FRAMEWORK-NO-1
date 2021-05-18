c781ad38a8265cc81f8bc024c28351019c4454e2
laiba taha <77200349+Laibataha08@users.noreply.github.com>
2021-05-19 00:11:54 +0500

    my 6 files

---
+++ Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Collections;
+using System.IO;
+
+namespace WindowsFormsApp4
+{
+    class Game
+    {
+        public int speed;
+        public static ArrayList allObjects = new ArrayList();
+
+        public Game(int speed)
+        {
+            this.speed = speed;
+        }
+
+        public void AddObjects(GameObject G)
+        {
+            allObjects.Add(G);
+        }
+
+        public void update()
+        {
+            for (int idx = 0 ; idx < allObjects.Count; idx++)
+            {
+                GameObject g = (GameObject)allObjects[idx];
+                g.updateposition(speed);
+            }
+            
+        }
+    }
+}
---
+++ GameObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+namespace WindowsFormsApp4
+{
+    class GameObject
+    {
+
+        PictureBox pictureBox;
+
+        public GameObject(PictureBox pictureBox)
+        {
+            this.pictureBox = pictureBox;
+        }
+
+        public void updateposition(int speed)
+        {
+            pictureBox.Left += speed;
+            
+        }
+       
+
+    }
+}
